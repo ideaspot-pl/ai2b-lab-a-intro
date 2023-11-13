511b937c1aaceff05b8664242bdf0ed14cd15901
Artur Karczmarczyk <artur@ideaspot.pl>
2023-11-13 13:30:42 +0100

    step 7 - filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,9 +16,21 @@ import {HousingService} from "../housing.service";
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
